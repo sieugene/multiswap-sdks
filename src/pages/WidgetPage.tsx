01ad075cee4ce9ca55960662ea0bb0791f5939bb
sieugene <sieugene@mail.ru>
2022-08-30 18:10:53 +0300

    chore : lint

--- src/pages/WidgetPage.tsx
+++ src/pages/WidgetPage.tsx
@@ -2,7 +2,8 @@ import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 import { useMemo } from 'react';
 
 export const WidgetPage = () => {
-  const widgetConfig: WidgetConfig = useMemo(() => ({
+  const widgetConfig: WidgetConfig = useMemo(
+    () => ({
       integrator: 'Your dApp/company name',
       containerStyle: {
         border: `1px solid ${
@@ -13,13 +14,18 @@ export const WidgetPage = () => {
         borderRadius: '16px',
         display: 'flex',
       },
-	  fromChainId: 3, // Ropsten
-	  fromAmount: '10000000000000000000', // 10
-	  fromTokenAddress: '0xe71678794fff8846bff855f716b0ce9d9a78e844', // TEST Token
-	  toChainId: 4, // Rinkeby
-	  toTokenAddress: '0x9ac2c46d7acc21c881154d57c0dc1c55a3139198'
-	  
-    }), []);
+      fromChainId: 3, // Ropsten
+      fromAmount: '10000000000000000000', // 10
+      fromTokenAddress: '0xe71678794fff8846bff855f716b0ce9d9a78e844', // TEST Token
+      toChainId: 4, // Rinkeby
+      toTokenAddress: '0x9ac2c46d7acc21c881154d57c0dc1c55a3139198',
+    }),
+    []
+  );
 
-  return <><LiFiWidget config={widgetConfig} /></>
+  return (
+    <>
+      <LiFiWidget config={widgetConfig} />
+    </>
+  );
 };
--- src/shared/config/dapp.config.ts
+++ src/shared/config/dapp.config.ts
@@ -1,9 +1,9 @@
 import { BSCTestnet, Config, Rinkeby } from '@usedapp/core';
 
 export const dappConfig: Config = {
-	readOnlyChainId: BSCTestnet.chainId,
-	readOnlyUrls: {
-	  [BSCTestnet.chainId]: "https://data-seed-prebsc-2-s2.binance.org:8545",
-	  [Rinkeby.chainId]: "https://rpc.ankr.com/eth_rinkeby",
-	},
-  }
+  readOnlyChainId: BSCTestnet.chainId,
+  readOnlyUrls: {
+    [BSCTestnet.chainId]: 'https://data-seed-prebsc-2-s2.binance.org:8545',
+    [Rinkeby.chainId]: 'https://rpc.ankr.com/eth_rinkeby',
+  },
+};
