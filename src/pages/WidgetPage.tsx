13c7b41d4d1b80526f0765e2abe2946fbf08e423
sieugene <sieugene@mail.ru>
2022-08-31 12:27:21 +0300

    chore : widget

--- src/pages/WidgetPage.tsx
+++ src/pages/WidgetPage.tsx
@@ -5,11 +5,12 @@ import LIFI, {
   RoutesRequest,
   ChainId,
 } from '@lifi/sdk';
+import { LiFiWidget, LiFiWidgetDrawer, WidgetConfig } from '@lifi/widget';
 import { BSCTestnet, Rinkeby, useEthers, useTokenBalance } from '@usedapp/core';
 import axios from 'axios';
 import { ethers } from 'ethers';
 import { formatEther, parseEther } from 'ethers/lib/utils';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export const WidgetPage = () => {
   const { library, account } = useEthers();
@@ -63,6 +64,26 @@ export const WidgetPage = () => {
     });
   };
 
+  const widgetConfig: WidgetConfig = useMemo(
+    () => ({
+      integrator: 'Your dApp/company name',
+      containerStyle: {
+        border: `1px solid ${
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'rgb(66, 66, 66)'
+            : 'rgb(234, 234, 234)'
+        }`,
+        borderRadius: '16px',
+        display: 'flex',
+      },
+      fromChain,
+      toChain,
+      toToken: tokens.to,
+      fromToken: tokens.from,
+    }),
+    []
+  );
+
   return (
     <>
       <p>
@@ -80,6 +101,7 @@ export const WidgetPage = () => {
         }}
       />
       <button onClick={make}>swap</button>
+      <LiFiWidgetDrawer config={widgetConfig} />;
     </>
   );
 };
